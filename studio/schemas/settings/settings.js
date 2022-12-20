9d8e71d05d4ad1fe1475ef7ffa825a9021fbdab5
Larsh95 <lars7537@easv365.dk>
2022-12-20 11:35:06 +0100

    settings

--- studio/schemas/settings/settings.js
+++ studio/schemas/settings/settings.js
@@ -46,9 +46,76 @@ export default {
       },
     },
     {
-      title: 'Site settings',
-      name: 'siteSettings',
-      type: 'siteConfig',
+      title: 'Company name',
+      name: 'companyName',
+      type: 'string',
+      fieldset: 'contact',
+    },
+    {
+      title: 'CVR',
+      name: 'cvr',
+      type: 'string',
+      fieldset: 'contact',
+      validation: (Rule) =>
+        Rule.min(8)
+          .max(8)
+          .warning(`A CVR number cannot be more or less than 8 characters.`),
+    },
+    {
+      title: 'Address',
+      name: 'address',
+      type: 'string',
+      fieldset: 'contact',
+    },
+    {
+      title: 'Postal Code',
+      name: 'postalCode',
+      type: 'string',
+      fieldset: 'contact',
+      validation: (Rule) =>
+        Rule.min(4)
+          .error('Postal codes must have a minimum of 4 characters')
+          .max(4)
+          .error('Postal codes have a maximum of 4 characters'),
+    },
+    {
+      title: 'Email',
+      name: 'email',
+      type: 'string',
+      fieldset: 'contact',
+      validation: (Rule) => Rule.email(),
+    },
+    {
+      title: 'Phone',
+      name: 'phone',
+      type: 'string',
+      description: 'International phone number ex. +45 xx xx xx xx',
+      fieldset: 'contact',
+      validation: (Rule) =>
+        Rule.custom((phone) => {
+          if (typeof phone === 'undefined') {
+            return true // Allow undefined values, remove if the field is required
+          }
+          const regex =
+            /^\+\d{1,3}[\s-]?\d{1,3}[\s-]?\d{1,4}[\s-]?\d{1,4}[\s-]?\d{1,9}$/ // Regex pattern goes here
+          if (regex.test(phone)) {
+            return true
+          } else {
+            return 'Not a valid international phone number' // Error message goes here
+          }
+        }),
+    },
+    {
+      title: 'Instagram',
+      name: 'instagram',
+      type: 'string',
+      fieldset: 'SoMe',
+    },
+    {
+      title: 'Facebook',
+      name: 'facebook',
+      type: 'string',
+      fieldset: 'SoMe',
     },
   ],
 }
