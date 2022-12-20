6f886b000d527a73ce428a27b20504b061356baf
Larsh95 <lars7537@easv365.dk>
2022-12-20 11:26:59 +0100

    settings

---
+++ studio/schemas/settings/siteConfig.js
@@ -0,0 +1,115 @@
+/*
+Comments
+Needs to be added. 
+Site settings:
+Firmanavn
+adresse
+cvr
+email
+mobil
+
+socialemedier fb,insta
+*/
+
+export default {
+  title: 'Site Settings',
+  name: 'siteConfig',
+  type: 'object',
+  groups: [
+    { title: 'General', name: 'general', default: true },
+    { title: 'Styling', name: 'styling' },
+    { title: 'Seo', name: 'seo' },
+  ],
+  fieldsets: [
+    {
+      title: '',
+      name: 'contact',
+      options: { columns: 2 },
+    },
+    {
+      title: '',
+      name: 'SoMe',
+      options: { columns: 2 },
+    },
+  ],
+  fields: [
+    {
+      title: 'Company name',
+      name: 'companyName',
+      type: 'string',
+      fieldset: 'contact',
+    },
+    {
+      title: 'CVR',
+      name: 'cvr',
+      type: 'string',
+      fieldset: 'contact',
+      validation: (Rule) =>
+        Rule.min(8)
+          .max(8)
+          .error(`A CVR number cannot be more or less than 8 characters.`),
+    },
+    {
+      title: 'Address',
+      name: 'address',
+      type: 'string',
+      fieldset: 'contact',
+    },
+    {
+      title: 'Postal Code',
+      name: 'postalCode',
+      type: 'string',
+      fieldset: 'contact',
+      validation: (Rule) =>
+        Rule.min(4)
+          .error('Postal codes must have a minimum of 4 characters')
+          .max(4)
+          .error('Postal codes have a maximum of 4 characters'),
+    },
+    {
+      title: 'Email',
+      name: 'email',
+      type: 'string',
+      fieldset: 'contact',
+      validation: (Rule) => Rule.email(),
+    },
+    {
+      title: 'Phone',
+      name: 'phone',
+      type: 'string',
+      description: 'International phone number ex. +45 xx xx xx xx',
+      fieldset: 'contact',
+      validation: (Rule) =>
+        Rule.custom((phone) => {
+          if (typeof phone === 'undefined') {
+            return true // Allow undefined values, remove if the field is required
+          }
+          const regex =
+            /^\+\d{1,3}[\s-]?\d{1,3}[\s-]?\d{1,4}[\s-]?\d{1,4}[\s-]?\d{1,9}$/ // Regex pattern goes here
+          if (regex.test(phone)) {
+            return true
+          } else {
+            return 'Not a valid international phone number' // Error message goes here
+          }
+        }),
+    },
+    {
+      title: 'Instagram',
+      name: 'instagram',
+      type: 'string',
+      fieldset: 'SoMe',
+    },
+    {
+      title: 'Facebook',
+      name: 'facebook',
+      type: 'string',
+      fieldset: 'SoMe',
+    },
+    {
+      title: 'Custom Class',
+      name: 'customClass',
+      type: 'string',
+      group: 'settings',
+    },
+  ],
+}
