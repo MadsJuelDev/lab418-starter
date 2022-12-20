eda9a0cab77927df1ad54c46ce0cc7b9e09a4341
Larsh95 <lars7537@easv365.dk>
2022-12-20 11:16:11 +0100

    fix for frontend

--- src/components/modules/RichTextEditor.js
+++ src/components/modules/RichTextEditor.js
@@ -3,25 +3,18 @@ import PortableText from 'react-portable-text'
 const RichTextEditor = ({ data }) => {
   return (
     <div className={data.customClass}>
-      <PortableText
-        className="richText"
-        dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}
-        projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
-        content={data.content}
-        serializers={{
-          h1: ({ children }) => (
-            <h1>
-              {children}
-            </h1>
-          ),
-          link: ({ children, href }) => (
-          <a href={href}>
-            {children}
-          </a>
-          )
-          
-        }}
-      />
+      {data.content && (
+        <PortableText
+          className='richText'
+          dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}
+          projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}
+          content={data.content}
+          serializers={{
+            h1: ({ children }) => <h1>{children}</h1>,
+            link: ({ children, href }) => <a href={href}>{children}</a>,
+          }}
+        />
+      )}
     </div>
   )
 }
