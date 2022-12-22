4ba901ee4cd2c7031c1b8c5eb7ef93944220c182
Mads Juel <madsjuel93@gmail.com>
2022-12-22 09:41:07 +0100

    Sanity security lol

--- src/utils/sanityFetch/sanity.js
+++ src/utils/sanityFetch/sanity.js
@@ -1,10 +1,10 @@
 import { createClient } from 'next-sanity'
 import createImageUrlBuilder from '@sanity/image-url'
 
-const sanityId = 'guc44bdp'
+const sanityId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID //'guc44bdp'
 
 const config = {
-  dataset: 'production',
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   projectId: sanityId,
   apiVersion: '2022-11-17',
   useCdn: false,
